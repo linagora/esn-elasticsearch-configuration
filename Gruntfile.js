692b24dccee1b70f34afd81579d2617ec9047057
Christophe Hamerling <chamerling@linagora.com>
2016-02-15 17:35:50 +0100

    OR-2107 Bootstrap utility to create ES index

---
+++ Gruntfile.js
@@ -0,0 +1,137 @@
+'use strict';
+
+module.exports = function(grunt) {
+  var CI = grunt.option('ci');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    project: {
+      lib: 'lib',
+      test: 'test',
+      dist: 'dist',
+      bin: 'bin',
+      data: 'data',
+      name: 'esn-elasticsearch-configuration'
+    },
+
+    babel: {
+      options: {
+        sourceMap: false,
+        presets: ['es2015']
+      },
+      dist: {
+        files: [
+          {
+            expand: true,
+            src: [
+              '<%= project.lib %>/**/*.js',
+              '<%= project.bin %>/**/*.js'
+            ],
+            dest: '<%= project.dist %>/'
+          }
+        ]
+      }
+    },
+
+    copy: {
+      data: {
+        files: [
+          {
+            expand: true,
+            src: ['<%= project.data %>/*.json'],
+            dest: '<%= project.dist %>/',
+            filter: 'isFile'
+          }
+        ],
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: CI && 'checkstyle',
+        reporterOutput: CI && 'jshint.xml'
+      },
+      all: {
+        src: [
+          'Gruntfile.js',
+          '<%= project.test %>/**/*.js',
+          '<%= project.lib %>/**/*.js',
+          '<%= project.bin %>/**/*.js'
+        ]
+      }
+    },
+
+    jscs: {
+      lint: {
+        options: {
+          config: '.jscsrc',
+          esnext: true
+        },
+        src: ['<%= jshint.all.src %>']
+      },
+      fix: {
+        options: {
+          config: '.jscsrc',
+          esnext: true,
+          fix: true
+        },
+        src: ['<%= jshint.all.src %>']
+      }
+    },
+
+    lint_pattern: {
+      options: {
+        rules: [
+          { pattern: /(describe|it)\.only/, message: 'Must not use .only in tests' }
+        ]
+      },
+      all: {
+        src: ['<%= jshint.all.src %>']
+      }
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '<%= project.dist %>/*',
+            '!<%= project.dist %>/.git*'
+          ]
+        }]
+      }
+    },
+    release: {
+      options: {
+        file: 'package.json',
+        commitMessage: 'Bumped version to <%= version %>',
+        tagName: 'v<%= version %>',
+        tagMessage: 'Version <%= version %>',
+        afterBump: ['exec:gitcheckout_ReleaseBranch', 'test', 'apidoc'],
+        afterRelease: ['exec:gitcheckout_master']
+      }
+    },
+
+    exec: {
+      gitcheckout_ReleaseBranch: {
+        cmd: function() {
+          return 'git checkout -b release-' + this.file.readJSON('package.json').version;
+        }
+      },
+      gitcheckout_master: {
+        cmd: 'git checkout master'
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('compile', 'Compile from ES6 to ES5', ['clean:dist', 'copy:data', 'babel']);
+  grunt.registerTask('linters', 'Check code for lint', ['jshint:all', 'jscs:lint', 'lint_pattern:all']);
+  grunt.registerTask('package', 'Package module', ['clean:dist', 'linters', 'copy:data', 'babel']);
+  grunt.registerTask('default', ['linters', 'compile']);
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Elasticsearch Configuration
+
+ES configuration utility for OpenPaaS ESN.
+
+The current package contains:
+
+- Indexes configuration in data folder
+- A CLI to create indexes
+
+## Create indexes from CLI
+
+Install the CLI from NPM
+
+``` bash
+npm install -g esn-elasticsearch-configuration
+```
+
+Then use it
+
+```bash
+esn-elasticsearch-configuration index contacts --host 192.168.99.100 --port 9200
+```
+
+Will create the contacts index on ES running on 192.168.99.100:9200.
+Full help is available 'esn-elasticsearch-configuration --help'
+
+## License
+
+MIT
