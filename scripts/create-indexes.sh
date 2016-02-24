3f3f63168dff8053f0db51a5b60a024a55912df9
Christophe Hamerling <chamerling@linagora.com>
2016-02-24 23:15:35 +0100

    OR-2098 Fix script data path

--- scripts/create-indexes.sh
+++ scripts/create-indexes.sh
@@ -2,12 +2,13 @@
 
 es_host="localhost"
 es_port="9200"
+current_path="$(dirname "$0")"
 
 [ -z "$ELASTICSEARCH_HOST" ] || es_host="$ELASTICSEARCH_HOST"
 [ -z "$ELASTICSEARCH_PORT" ] || es_port="$ELASTICSEARCH_PORT"
 
 echo -e "Setting up users index"
-curl -s -i -XPUT "${es_host}:${es_port}/users.idx" -d @./../data/users.json
+curl -s -i -XPUT "${es_host}:${es_port}/users.idx" -d @${current_path}/../data/users.json
 
 echo -e "Setting up contacts index"
-curl -s -i -XPUT "${es_host}:${es_port}/contacts.idx" -d @./../data/contacts.json
+curl -s -i -XPUT "${es_host}:${es_port}/contacts.idx" -d @${current_path}/../data/contacts.json
